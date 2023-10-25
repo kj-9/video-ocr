c8b1f5dd355f7e60b0933763932b7711217c81d5
kj-9 <46800811+kj-9@users.noreply.github.com>
2023-10-25 20:06:11 +0900

    update function

--- video_ocr/execute.py
+++ video_ocr/execute.py
@@ -1,11 +1,11 @@
 import timeit
+import typing as t
 from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
 from time import monotonic
 
 from serde import SerdeError
 
 from video_ocr.config import get_logger
-from video_ocr.playlist import Playlist
 from video_ocr.video import Video
 
 logger = get_logger(__name__)
@@ -38,36 +38,39 @@ def _load_or_create_video(video_id: str, load=False) -> Video:
     return video
 
 
-def _save_frames(video):
+def _to_frames_if_not_exists(video: Video) -> None:
     if not video.frames:
         video.to_frames()
         logger.info(f"{video.to_json()=}")
 
 
-def get_func_save_frames(load=True):
-    def closure(video_id):
+def func_to_frames_if_not_exists(load=True) -> t.Callable:
+    def closure(video_id: str):
         video = _load_or_create_video(video_id, load=load)
-        _save_frames(video)
+        _to_frames_if_not_exists(video)
 
     return closure
 
 
-def _save_ocr_results(video):
+def _run_ocr_if_not_exists(video: Video) -> None:
     results = [frame.results for frame in video.frames]
 
     if any(results):
+        logger.info(f"{video.video_id=} has already saved results, stop ocr.")
         # already saved results
         return
 
+    logger.info(f"{video.video_id=} has no saved results, run ocr.")
     video.get_frames_ocr()
     logger.info(f"{video.to_json()=}")
-    return video
 
 
-def get_func_save_results(load=True):
-    def closure(video_id):
-        video = _load_or_create_video(video_id, load=load)
-        return _save_ocr_results(video)
+def func_run_ocr_if_not_exists() -> t.Callable:
+    def closure(video_id: str):
+        video = _load_or_create_video(
+            video_id, load=True
+        )  # must load video with frames
+        _run_ocr_if_not_exists(video)
 
     return closure
 
@@ -92,16 +95,16 @@ def log_time(function):
     return wrap
 
 
-def run_ocr(video_ids: list[str], load=False) -> None:
-    start = timeit.default_timer()
-    run_on_thread_pool(get_func_save_frames(load), video_ids)
-    end = timeit.default_timer()
-    logger.info(f"finish save frames: {end - start}")
+def run_ocr_multi_process(video_ids: list[str], load=False) -> None:
+    logger.info("start saving frames.")
+    run_on_thread_pool(func_to_frames_if_not_exists(load), video_ids)
+    logger.info("finish saving frames.")
 
-    start = timeit.default_timer()
-    run_on_process_pool(get_func_save_results(load), video_ids)
-    end = timeit.default_timer()
-    logger.info(f"finish save ocr results: {end - start}")
+    logger.info("start saving ocr results.")
+    # must load=True, since we need to load generated frames
+    # or there will be no frames to ocr
+    run_on_process_pool(func_run_ocr_if_not_exists(), video_ids)
+    logger.info("finish saving ocr results.")
 
 
 def check_performance(
@@ -154,32 +157,3 @@ def check_performance(
         result["sync"] = end - start
 
     return result
-
-
-CHECK_PERFORMANCE = False
-if __name__ == "__main__":
-    playlist = Playlist.from_json()
-
-    logger.info(len(playlist.items))
-
-    video_ids = playlist.to_video_ids()
-    len_video_ids = len(video_ids)
-
-    logger.info(f"{len_video_ids=}")
-
-    start = timeit.default_timer()
-    run_on_thread_pool(get_func_save_frames(), video_ids)
-    end = timeit.default_timer()
-    logger.info(f"finish save frames: {end - start}")
-
-    start = timeit.default_timer()
-    run_on_process_pool(get_func_save_results(), video_ids)
-    end = timeit.default_timer()
-    logger.info(f"finish save ocr results: {end - start}")
-
-    if CHECK_PERFORMANCE:
-        # sampling
-        sample_video_ids = video_ids[:10]
-        # choose task
-        logger.info(f"{check_performance(sample_video_ids, get_func_save_frames())=}")
-        logger.info(f"{check_performance(sample_video_ids, get_func_save_frames())=}")
